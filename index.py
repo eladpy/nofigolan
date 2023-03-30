1bcb22caab39d33afa0264ee1a2ed1ce23590ca6
DESKTOP-435S5DU\User <elad.levy.2007@gmail.com>
2023-03-30 13:12:25 +0300

    add list of restaurants

--- index.py
+++ index.py
@@ -1,1 +1,8 @@
-restaurant = [dodis : 'givat']
+restaurants = [["liz", True, 'vegan'],
+               ["emma", 1.68],
+               ["mom", 1.71],
+               ["dad", 1.89]]
+restaurants
+print(restaurants[0][0])
+
+# def res_details()
